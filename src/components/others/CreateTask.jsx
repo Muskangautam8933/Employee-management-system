6467f4e304ff9f397286ec2a1970c16d53420658
Muskan Gautam <muskangautam7064@gmail.com>
2026-03-31 15:48:26 +0530

    update

--- src/components/others/CreateTask.jsx
+++ src/components/others/CreateTask.jsx
@@ -14,14 +14,14 @@ const CreateTask = () => {
   const submitHandler = (e) =>{
      e.preventDefault()
      
-    setNewTask({taskTitle,taskDescription,taskDate,category,active:false,newTask:true,failed:false,completed:false})
+    const newTaskObj = {taskTitle,taskDescription,taskDate,category,active:false,newTask:true,failed:false,completed:false}
 
     const data = userData
     
     
     data.forEach(function (elem) {
       if(assignTo == elem.firstName){
-        elem.tasks.push(newTask)
+        elem.tasks.push(newTaskObj)
         elem.taskCounts.newTask = elem.taskCounts.newTask+1
       }
     })
--- src/components/others/TaskListNumber.jsx
+++ src/components/others/TaskListNumber.jsx
@@ -9,15 +9,15 @@ const TaskListNumber = ({data}) => {
         </div>
         <div className='p-10 rounded-xl w-[45%] py-6 px-9 bg-blue-400 '>
           <h2 className='text-3xl font-semibold'>{data.taskCounts.completed}</h2>
-          <h3 className='text-xl font-medium'>New Task</h3>
+          <h3 className='text-xl font-medium'>Completed</h3>
         </div>
         <div className='p-10 rounded-xl w-[45%] py-6 px-9 bg-green-400 '>
           <h2 className='text-3xl font-semibold'>{data.taskCounts.active}</h2>
-          <h3 className='text-xl font-medium'>New Task</h3>
+          <h3 className='text-xl font-medium'>Active</h3>
         </div>
         <div className='p-10 rounded-xl w-[45%] py-6 px-9 bg-purple-400 '>
           <h2 className='text-3xl font-semibold'>{data.taskCounts.failed}</h2>
-          <h3 className='text-xl font-medium'>New Task</h3>
+          <h3 className='text-xl font-medium'>Failed</h3>
         </div>
     </div>
   );
